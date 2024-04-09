0c3e57a8ad2af3d090eee9484ef2d0a0bc18df2a
chuy-zip <andreschuy03@gmail.com>
2024-04-09 15:12:43 -0600

    minor change in schema.sqñ

--- vite-project/src/postgreSQL/schema.sql
+++ vite-project/src/postgreSQL/schema.sql
@@ -70,7 +70,8 @@ CREATE TABLE IF NOT EXISTS orden (
 CREATE TABLE IF NOT EXISTS contenido_orden (
     id_orden INTEGER NOT NULL,
     cantidad_producto NUMERIC(4) NOT NULL,
-    id_producto INTEGER NOT NULL
+    id_producto INTEGER NOT NULL,
+    completado BOOLEAN NOT NULL 
 );
 
 CREATE TABLE IF NOT EXISTS factura (
