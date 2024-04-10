9d6ef532777734de81e410bc53a65b211070f4f2
DANdelion <pen22217@uvg.edu.gt>
2024-04-10 14:29:10 -0600

    Create controller, example object, component and menu view
    Unfinished.

---
+++ vite-project/src/controller/menuController.js
@@ -0,0 +1,41 @@
+async function getDishes() {
+    const dishesExample = [
+        {
+            "id": 0,
+            "nombre": "Pizza Familiar",
+            "descripcion": "Una enorme pizza preparada con ingredientes frescos sobre una masa crujiente.",
+            "precio": 100.00
+        },
+
+        {
+            "id": 1,
+            "nombre": "Hamburguesa Bomba",
+            "descripcion": "Hamburguesa que incluye una jugosa torta de carne de res, vegetales frescos y pan Premium.",
+            "precio": 60.00 
+        },
+
+        {
+            "id": 2,
+            "nombre": "Pasta a la Bolognesa",
+            "descripcion": "Pasta corta preparada con nuestra salsa de tomate especial y albóndigas de carne.",
+            "precio": 65.00 
+        },
+
+        {
+            "id": 3,
+            "nombre": "Tapao",
+            "descripcion": "Delicioso caldo de mariscos preparado con leche de coco y acompañado de rice and bean.",
+            "precio": 80.00 
+        },
+
+        {
+            "id": 4,
+            "nombre": "Cereal con leche",
+            "descripcion": "Mítico bowl lleno de leche y acompañado del cereal de tu preferencia.",
+            "precio": 20.00 
+        },
+
+    ]
+
+    
+}
---
+++ vite-project/src/frontend/views/menu.jsx
@@ -0,0 +1,3 @@
+import { useState } from "react";
+import ImageCard from "../components/ImageCard";
+import logo from '../../imgs/dans_logo.png'
