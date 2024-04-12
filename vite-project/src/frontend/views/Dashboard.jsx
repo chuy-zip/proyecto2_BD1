0d93893262a8c8e38b6005ce6ad80bdd8794b0c4
chuy-zip <andreschuy03@gmail.com>
2024-04-12 13:38:34 -0600

    Added minor style changes

--- vite-project/src/frontend/views/Dashboard.jsx
+++ vite-project/src/frontend/views/Dashboard.jsx
@@ -1,17 +1,20 @@
 function DashBoard({navigate}){
     return (
         <>
-            <div>
+            <div className="kitbar">
+                <div>
+                    <h1 className="viewTittle">Bienvenido</h1>
+                    
+                    <a href="javascript:void(0);" onClick={ () => navigate("kitchen")}>Cocina</a> | 
+                    <a href="javascript:void(0);" onClick={ () => navigate("bar")}> Bar</a> | 
+                    <a href="javascript:void(0);" onClick={ () => navigate("searchOrder")}> Buscar Orden</a> | 
+                    <a href="javascript:void(0);" onClick={ () => navigate("menu")}> Menu</a> |                     
+                    
+                    <p>Prueba de navegacion</p>
 
-                <h1>DashBoard</h1>
+                </div>
+
                 
-                <a href="javascript:void(0);" onClick={ () => navigate("kitchen")}>Cocina</a> | 
-                <a href="javascript:void(0);" onClick={ () => navigate("bar")}> Bar</a> | 
-                <a href="javascript:void(0);" onClick={ () => navigate("searchOrder")}> Buscar Orden</a> | 
-                <a href="javascript:void(0);" onClick={ () => navigate("menu")}> Menu</a> | 
-                
-                
-                <p>Prueba de navegacion</p>
             </div>
         </>
         
