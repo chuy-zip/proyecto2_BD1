978abdfa5ffeb8b92c70dd5c8ca485cde3a3ba4b
chuy-zip <andreschuy03@gmail.com>
2024-04-12 17:17:21 -0600

    Improved Dashboard

--- vite-project/src/frontend/views/Dashboard.jsx
+++ vite-project/src/frontend/views/Dashboard.jsx
@@ -1,50 +1,50 @@
 import '../styles/Dashboard.css'
 function DashBoard({navigate}){
     return (
-        <>
-            <div className="kitbar">
+            <div className='dashboard-Container'>
+                <nav>
+                    <a href="javascript:void(0);" onClick={ () => navigate("kitchen")}>Cocina</a>
+                    <a href="javascript:void(0);" onClick={ () => navigate("bar")}> Bar</a>
+                    <a href="javascript:void(0);" onClick={ () => navigate("searchOrder")}> Buscar Orden</a>
+                    <a href="javascript:void(0);" onClick={ () => navigate("menu")}> Menu</a>                                     
+                </nav>
                 <div>
-                    <nav>
-                        <a href="javascript:void(0);" onClick={ () => navigate("kitchen")}>Cocina</a>
-                        <a href="javascript:void(0);" onClick={ () => navigate("bar")}> Bar</a>
-                        <a href="javascript:void(0);" onClick={ () => navigate("searchOrder")}> Buscar Orden</a>
-                        <a href="javascript:void(0);" onClick={ () => navigate("menu")}> Menu</a>                                     
-                    </nav>
+                    <div>
+                        
+                        
+                        <h1 className="viewTittle">Bienvenido</h1>
+                        
+
+                    </div>
+
+                    <div className="dropDown">
+                        <select
+                            className="dropdown-select"
+                        >
+                            <option value="">Elegir un reporte</option>
+                            <option value="reporte1">Platos mas pedidos</option>
+                            <option value="reporte2">Horario con mas pedidos</option>
+                            <option value="reporte3">Promedio de tiempo de comer</option>
+                            <option value="reporte4">Quejas de Personas</option>
+                            <option value="reporte5">Quejas de Platos</option>
+                            <option value="reporte6">Eficiencia de mesesores</option>
+
+                        </select>
+
+                    </div>
                     
-                    <h1 className="viewTittle">Bienvenido</h1>
-                    
+                    <button 
+                        className='orderCompleteButton' 
+                        style={{ margin: 'auto', marginTop: '20px' }} 
+
+                        onClick={() => 
+                            console.log("Test for search button")
+                        }
+                        > Obtener reporte
+                    </button>
 
                 </div>
-
-                <div className="dropDown">
-                    <select
-                        className="dropdown-select"
-                    >
-                        <option value="">Elegir un tipo de reporte</option>
-                        <option value="reporte1">Platos mas pedidos</option>
-                        <option value="reporte2">Horario con mas pedidos</option>
-                        <option value="reporte3">Promedio de tiempo de comer</option>
-                        <option value="reporte4">Quejas de Personas</option>
-                        <option value="reporte5">Quejas de Platos</option>
-                        <option value="reporte6">Eficiencia de mesesores</option>
-
-                    </select>
-
-                </div>
-                
-                <button 
-                    className='orderCompleteButton' 
-                    style={{ margin: 'auto', marginTop: '20px' }} 
-
-                    onClick={() => 
-                        console.log("Test for search button")
-                    }
-                    > Obtener reporte
-                </button>
-
             </div>
-        </>
-        
     )
 }
 
