630ed7cfbc3f1fd478c01b219160b68c025c255d
chuy-zip <andreschuy03@gmail.com>
2024-04-14 21:50:45 -0600

    First version for employee complaints

--- vite-project/src/frontend/views/Reports/EmployeeComplaints.jsx
+++ vite-project/src/frontend/views/Reports/EmployeeComplaints.jsx
@@ -3,13 +3,15 @@ import { getComplaintsByUser } from "../../../controller/complaintsController"
 
 function EmployeeComplaints(){
     const [complaints, setComplaints] = useState({})
-    const [initialDate, serInitialDate] = useState("")
-    const [endDate, serEndDate] = useState("")
+    const [initialDate, setInitialDate] = useState("")
+    const [endDate, setEndDate] = useState("")
 
     useEffect ( () => {
         const fetchComplaints = async () => {
             try {
                 let initialComplaints = await getComplaintsByUser('2020-01-01','2025-12-31')
+                initialComplaints = JSON.parse(initialComplaints)
+                setComplaints(initialComplaints)
             } catch (error) {
                 console.error('Error fetching complaints:', error);
             }
@@ -18,10 +20,52 @@ function EmployeeComplaints(){
         fetchComplaints()
     }, []);
 
+    const initialDateChange = (event) => {
+        setInitialDate(event.target.value)
+        
+    }
+
+    const endDateChange = (event) => {
+        setEndDate(event.target.value)
+        
+    }
+
     return (
     
-        <div> 
-            rep4
+        <div 
+            className="order"
+            style={{width:'75%'}}> 
+            <h1> Reporte de quejas agrupadas por empleados </h1>
+            
+            <h3> Fecha inicial</h3>
+
+            <input 
+                className="tableOrderInput" 
+                type="date" 
+                id="initialdate" 
+                value={initialDate} 
+                onChange={initialDateChange}
+                style={{ marginTop: '5px' }}></input>
+
+            <h3> Fecha Final</h3>
+
+            <input 
+                className="tableOrderInput" 
+                type="date" 
+                id="enddate" 
+                value={endDate} 
+                onChange={endDateChange}
+                style={{ marginTop: '5px' }}></input>
+
+            <div 
+                className="buttonContainer"
+                style={{ margin: 'auto', marginBottom: '20px' }} >
+                <button 
+                    className='orderCompleteButton' 
+                    style={{ margin: 'auto', marginTop: '20px' }} 
+                    onClick={() => console.log(initialDate, endDate)}>Generar Reporte</button>
+            </div>
+
         </div>
     )
 }
